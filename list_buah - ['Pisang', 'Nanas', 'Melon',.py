a289a96536051807db32cee137e72f402c07b9ce
fatrimulia28-oss <fatrimulia28@gmail.com>
2025-10-08 11:20:26 +0700

    Add files via upload

---
+++ list_buah - ['Pisang', 'Nanas', 'Melon',.py
@@ -0,0 +1,9 @@
+list_buah - ['Pisang', 'Nanas', 'Melon', 'Durian']
+
+print(list_buah[0:1])
+print(list_buah[0:2])
+print(list_buah[1:3])
+print(list_buah[0:-1])
+print(list_buah[-1:-3])
+print(list_buah[-1:3])
+print(list_buah[-3:-1])
