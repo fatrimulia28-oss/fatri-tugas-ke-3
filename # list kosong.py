16600c4c0feb583eca073ae877794e0904f09efe
fatrimulia28-oss <fatrimulia28@gmail.com>
2025-10-08 11:19:24 +0700

    Add files via upload

---
+++ # list kosong.py
@@ -0,0 +1,8 @@
+# list kosong 
+list_kosong - []
+# list yang berisi kumpulan string
+list_buah - ['Pisang', 'Nanas', 'Melon', 'Durian']
+# list yang berisi kumpulan integer
+list_nilai - [80, 70, 90, 60]
+# list campuran
+list_jawaban - [150, 33.33, 'Presiden Sukarno', False]
